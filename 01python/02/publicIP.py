af03ef4d9b2c64b19617de16c0a5792ad953f4a7
SalmaMohammedHamedMustafa <salmamohammedhamed2@gmail.com>
2024-06-25 13:36:21 +0300

    public ip task

--- 01python/02/publicIP.py
+++ 01python/02/publicIP.py
@@ -1,10 +1,17 @@
 import requests
 
 def get_public_ip():
+    """
+    gets the public IP address of the machine by making an HTTP request to 'https://api.ipify.org/?format=json', which returns the public IP address in JSON format. If the request is successful, it extracts and returns the IP address from the JSON response. In case of any request exceptions, it prints an error message and returns None.
+
+    Returns:
+    str: The public IP address as a string if the request is successful.
+    None: If there is an error while making the request.
+    """
     try:
-        response = requests.get('https://api.ipify.org?format=json')
+        response = requests.get('https://api.ipify.org/?format=json')
         ip_info = response.json()
-        return ip_info['ip']
+        return ip_info
     except requests.RequestException as e:
         print(f"Error fetching public IP: {e}")
         return None
@@ -12,4 +19,4 @@ def get_public_ip():
 
 public_ip = get_public_ip()
 if public_ip:
-    print(f"My public IP address is: {public_ip}")
+    print(public_ip)
