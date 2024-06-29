18a6cf000859fdb70f0aa16866021b6b86b017d7
SalmaMohammedHamedMustafa <salmamohammedhamed2@gmail.com>
2024-06-29 22:48:44 +0300

    Alexa v1

--- 01python/Alexa/Alexa.py
+++ 01python/Alexa/Alexa.py
@@ -1,4 +1,3 @@
-# import library
 import webbrowser
 from time import ctime
 from datetime import datetime
@@ -7,8 +6,8 @@ import playsound
 from gtts import gTTS
 import random
 import speech_recognition as sr
-import threading
 import firelink
+import sys
 
 class voice_assistant:
     recognizer = sr.Recognizer()
@@ -24,7 +23,6 @@ class voice_assistant:
         text = ""
         try:
             text = self.recognizer.recognize_google(audio, language="ar-EG")
-            # text = self.recognizer.recognize_whisper(audio, language="arabic")
             print(f"You said: {text}")
         except sr.UnknownValueError:
             print("Sorry, I couldn't understand that.")
@@ -37,47 +35,53 @@ class voice_assistant:
         tts.save("audio.mp3")
         playsound.playsound("audio.mp3")
 
-
-
-    def open_link (self,text):
-        websites ={
-        "فيسبوك": firelink.facebook_link,
-        "جوجل": firelink.google_link,
-        "يوتيوب": firelink.youtube_link,
-        "تويتر": firelink.twitter_link
-        }  
+    def open_link(self):
+        self.speak('اختار الموقع')
+        print("Say...")
+        audio_data = self.record_audio()
+        print("Recognizing speech...")
+        text = self.recognize_speech(audio_data)
+        websites = {
+            "فيسبوك": firelink.facebook_link,
+            "جوجل": firelink.google_link,
+            "يوتيوب": firelink.youtube_link,
+            "تويتر": firelink.twitter_link
+        }
         if text in websites:
             url = websites[text]
             firelink.firefox(url)
         else:
             print("Invalid choice. Please try again.")
 
-
-    
     def get_current_time(self):
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
         print(current_time)
-        assistant.speak(current_time)
+        self.speak(current_time)
 
+    def greeting(self, text):
+        if text in ['صباح الخير', 'صباح الفل', 'صباح النور']:
+            self.speak('صباح النور')
+        elif text in ['مساء الخير', 'مساء الفل', 'مساء النور']:
+            self.speak('مساء النور')
+        else:
+            self.speak('اهلا')
 
-    #def get_weather(self):
-
-
+    def Alexa(self):
+        print("Say something...")
+        audio_data = self.record_audio()
+        print("Recognizing speech...")
+        recognized_text = self.recognize_speech(audio_data)
+        if recognized_text in ['صباح الخير', 'صباح الفل', 'صباح النور', 'مساء الخير', 'مساء الفل', 'مساء النور', 'اهلا', 'هاي', 'هالو']:
+            self.greeting(recognized_text)
+        elif recognized_text in ["الوقت", "الساعة كام", "ساعة", "الساعة"]:
+            self.get_current_time()
+        elif recognized_text in ["موقع", "افتحي موقع","افتحي موقع", "ويب سايت"]:
+            self.open_link()
+        elif recognized_text in ["اقفل", "اقفلي", "close", "اطفي"]:
+            sys.exit()
 
 if __name__ == "__main__":
     assistant = voice_assistant()
-    
-    print("Say something...")
-    audio_data = assistant.record_audio()
-    
-    print("Recognizing speech...")
-    recognized_text = assistant.recognize_speech(audio_data)
-
-    #assistant.open_link(recognized_text)
-    if(recognized_text== "الوقت" or "الساعة كام" or "ساعة" or "الساعة"):
-        assistant.get_current_time();
-    
-    if recognized_text:
-        print("Speaking out the recognized text...")
-        assistant.speak(recognized_text)
+    while True:
+        assistant.Alexa()
