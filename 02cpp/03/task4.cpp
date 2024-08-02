45893acfca0bac09b1080bd5aa0487a05cbb2024
SalmaMohammedHamedMustafa <salmamohammedhamed2@gmail.com>
2024-08-02 23:44:50 +0300

    task4

---
+++ 02cpp/03/task4.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // Include for input and output operations
+
+int main() {
+    int a = 10; // Initialize variable 'a' with the value 10
+    if (a < 20 or a > 5) { // Check if 'a' is less than 20 and greater than 5
+        std::cout << "Yes"; // Output "Yes" if the condition is true
+    }
+    return 0; // Return 0 to signal successful completion
+}
